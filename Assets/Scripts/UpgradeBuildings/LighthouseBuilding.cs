9a7b768855b5a33093a8e2205a512813e4b04b4e
Mesdwe <40878295+Mesdwe@users.noreply.github.com>
2021-11-08 12:56:00 +0000

    buildings data

---
+++ Assets/Scripts/UpgradeBuildings/LighthouseBuilding.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Buildings/Lighthouse")]
+
+public class LighthouseBuilding : Building
+{
+    public LightHouseAbility[] lighthouseAbilities;
+}
--- Assets/Scripts/UpgradeBuildings/UpgradePanelUI.cs
+++ Assets/Scripts/UpgradeBuildings/UpgradePanelUI.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class UpgradePanelUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TextMeshProUGUI nameTMP;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
